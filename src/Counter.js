d76631239d3e19c16b649ffd66ce0349f735e90a
Coleman Escue <99232536+Nam3loc@users.noreply.github.com>
2022-12-06 13:54:13 -0500

    basic counter rendered

---
+++ src/Counter.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Counter() {
+    return (
+        <>
+            <span>Current Count: 0</span>
+            <section>
+                <button>+</button>
+                <button>-</button>
+            </section>
+        </>
+    )
+}
